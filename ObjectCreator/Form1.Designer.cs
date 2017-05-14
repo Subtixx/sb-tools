8021d03235de2bbae8c5b7176238088e1e230916
Subtixx <subtixx@gmail.com>
2017-05-14 07:27:30 +0200

    Finished UI

--- ObjectCreator/Form1.Designer.cs
+++ ObjectCreator/Form1.Designer.cs
@@ -40,22 +40,29 @@
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(563, 467);
+            this.button1.Location = new System.Drawing.Point(317, 381);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Write out";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 12);
+            this.textBox1.Location = new System.Drawing.Point(112, 6);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(280, 20);
             this.textBox1.TabIndex = 1;
@@ -69,7 +76,7 @@
             "Uncommon",
             "Rare",
             "Legendary "});
-            this.comboBox1.Location = new System.Drawing.Point(79, 38);
+            this.comboBox1.Location = new System.Drawing.Point(112, 32);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(280, 21);
             this.comboBox1.TabIndex = 2;
@@ -77,19 +84,20 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(79, 65);
+            this.listBox1.Location = new System.Drawing.Point(112, 59);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(280, 121);
             this.listBox1.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(206, 191);
+            this.button2.Location = new System.Drawing.Point(239, 185);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
             this.button2.Text = "Add";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBox2
             // 
@@ -219,23 +227,24 @@
             "wired",
             "wreck",
             "zen"});
-            this.comboBox2.Location = new System.Drawing.Point(79, 192);
+            this.comboBox2.Location = new System.Drawing.Point(112, 186);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 5;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(284, 191);
+            this.button3.Location = new System.Drawing.Point(317, 185);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 6;
             this.button3.Text = "Remove";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(79, 219);
+            this.textBox2.Location = new System.Drawing.Point(112, 213);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(280, 54);
@@ -243,7 +252,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(79, 279);
+            this.textBox3.Location = new System.Drawing.Point(112, 273);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(280, 20);
             this.textBox3.TabIndex = 8;
@@ -254,14 +263,14 @@
             this.comboBox3.FormattingEnabled = true;
             this.comboBox3.Items.AddRange(new object[] {
             "generic"});
-            this.comboBox3.Location = new System.Drawing.Point(79, 305);
+            this.comboBox3.Location = new System.Drawing.Point(112, 299);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(280, 21);
             this.comboBox3.TabIndex = 9;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(79, 332);
+            this.numericUpDown1.Location = new System.Drawing.Point(112, 326);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             999999999,
             0,
@@ -274,18 +283,88 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(79, 358);
+            this.checkBox1.Location = new System.Drawing.Point(112, 352);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(67, 17);
             this.checkBox1.TabIndex = 11;
             this.checkBox1.Text = "Printable";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Object Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Rarity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Colony Tags:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Description:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Short Description:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Race:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(67, 328);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Price:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(650, 502);
+            this.ClientSize = new System.Drawing.Size(402, 413);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.comboBox3);
@@ -298,6 +377,9 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
@@ -320,6 +402,13 @@
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- ObjectCreator/Form1.cs
+++ ObjectCreator/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -13,19 +14,48 @@ namespace ObjectCreator
         {
             InitializeComponent();
 
-            var objectText = File.ReadAllText(
+            /*var objectText = File.ReadAllText(
                                    @"D:\Program Files (x86)\Steam\steamapps\common\Starbound\moddev\extratransportation\objects\transport\hopper\hopper.object");
 
             ITraceWriter traceWriter = new MemoryTraceWriter();
-            dynamic obj = JsonConvert.DeserializeObject(objectText);
+            dynamic obj = JsonConvert.DeserializeObject(objectText);*/
             //StarboundObject obj = JsonConvert.DeserializeObject<StarboundObject>(objectText, new JsonSerializerSettings { TraceWriter = traceWriter });
             //Debug.WriteLine(obj);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (textBox1.Text == ""){ MessageBox.Show("Object name is required."); return; }
+
             StarboundObject obj = new StarboundObject(textBox1.Text);
+            if (comboBox1.SelectedItem != null)
+                obj.rarity = (string) comboBox1.SelectedItem;
+            if(listBox1.Items.Count > 0)
+                obj.colonyTags.AddRange(listBox1.Items.Cast<string>());
+            if (textBox2.Text != "")
+                obj.description = textBox2.Text;
+            if (textBox3.Text != "")
+                obj.shortdescription = textBox3.Text;
+            if (comboBox3.SelectedItem != null)
+                obj.race = (string)comboBox3.SelectedItem;
+            obj.price = (int)Math.Round(numericUpDown1.Value);
+            obj.printable = checkBox1.Checked;
+
             File.WriteAllText(textBox1.Text + ".object", JsonConvert.SerializeObject(obj, Formatting.Indented));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem == null) return;
+            if (listBox1.Items.Contains(comboBox2.SelectedItem)) return;
+
+            listBox1.Items.Add((string)comboBox2.SelectedItem);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItem != null)
+                listBox1.Items.Remove(listBox1.SelectedItem);
         }
     }
 }
